c38c72b1f28fa0b2b981a3457f8f8b0a7ffd3dd0
G060N0FF <georgi.hr.dim@gmail.com>
2020-06-01 22:37:12 +0300

    Update

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 class TodoForm(forms.Form):
     text=forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'formclass'}))
-    date=forms.DateField()
+    date=forms.DateField(label='', widget=forms.SelectDateWidget)
 
 class EditForm(forms.Form):
     text=forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'formclass'}), label='')
