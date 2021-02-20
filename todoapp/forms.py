5043fa0879999d96d8faac24364f93c40a2c5ab4
G060N0FF <georgi.hr.dim@gmail.com>
2021-02-20 18:08:15 +0200

    Design change

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -4,8 +4,8 @@ from django import forms
 
 # form to make a new to-do
 class TodoForm(forms.Form):
-    text = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'formclass'}))
-    date = forms.DateField(label='', widget=forms.SelectDateWidget)
+    text = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'formclass'}), label="Enter text:")
+    date = forms.DateField(widget=forms.SelectDateWidget, label='Enter Date:')
 
 
 # form to edit to-do
